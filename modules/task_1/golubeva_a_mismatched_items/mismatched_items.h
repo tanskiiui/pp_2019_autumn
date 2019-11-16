204dcab700e4b53686160797c733cd1480a0643c
tanskiiui <yurko1337@gmail.com>
2019-11-16 19:30:33 +0300

    test

--- modules/task_1/golubeva_a_mismatched_items/mismatched_items.h
+++
@@ -1,9 +0,0 @@
-//  Copyright 2019 Golubeva Anna
-#ifndef MODULES_TASK_1_GOLUBEVA_A_MISMATCHED_ITEMS_MISMATCHED_ITEMS_H_
-#define MODULES_TASK_1_GOLUBEVA_A_MISMATCHED_ITEMS_MISMATCHED_ITEMS_H_
-
-void getRandomStr(char *str, int size);
-int getCountDiffChar(char *str1, char *str2);
-int getCount(char* str1, char* str2);
-
-#endif  // MODULES_TASK_1_GOLUBEVA_A_MISMATCHED_ITEMS_MISMATCHED_ITEMS_H_
